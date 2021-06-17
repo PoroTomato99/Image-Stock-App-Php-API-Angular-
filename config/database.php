44f94f4aa71d8312c70c334d7307ce4193bf9883
PoroTomato99 <porotomato@gmail.com>
2021-06-17 22:40:13 +0800

    updated config file database.php

--- config/database.php
+++ config/database.php
@@ -66,9 +66,9 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'poto-api'),
+            'host' => env('DB_HOST'),
+            'port' => env('DB_PORT'),
+            'database' => env('DB_DATABASE'),
             'username' => env('DB_USERNAME'),
             'password' => env('DB_PASSWORD'),
             'charset' => 'utf8',
